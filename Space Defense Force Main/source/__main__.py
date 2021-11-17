c46deca92733ab910b35273fb9deec27585b5eb1
NemesisNeS <85811004+nemesisnes@users.noreply.github.com>
2021-11-17 17:20:50 -0500

    Add files via upload

---
+++ Space Defense Force Main/source/__main__.py
@@ -0,0 +1,15 @@
+import arcade
+from constants import *
+from start_view import StartView
+from window import Window
+
+def main():
+    # Load fonts
+    arcade.load_font("../fonts/arcade.ttf")
+    arcade.load_font("../fonts/SF Atarian System.ttf")
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = StartView()
+    window.show_view(start_view)
+    arcade.run()
+if __name__ == "__main__":
+    main()
